61514cd6ef04429bd5146ceeb662bd987c5f09f9
Fox <stufox@dca904921f38.ant.amazon.com>
2018-07-05 16:07:08 +1200

    Update top 20% graph

--- top20percent.cs
+++ top20percent.cs
@@ -32,34 +32,55 @@ namespace mbr
                 //var thisMonthCell = sheet.Cells[i,columns-1].Address;
                 var lastMonthCell = sheet.Cells[i,columns-3].Address;
                 sheet.Cells[i,columns].Formula = $"{changeCell}/{lastMonthCell}%";
-                //sheet.Cells[i,columns].Style.Numberformat.Format = "\"$\"#,##0.00";
+                sheet.Cells[i,columns].Style.Numberformat.Format = "0.00";
             }
             sheet.Calculate();
 
             // code goes here for sort 
             using (ExcelRange range = sheet.Cells[1,1,sheet.Dimension.End.Row,sheet.Dimension.End.Column])
             {
-                //range.Sort(range.Columns-1,Utils.sortDescending);
+                range.Sort(range.Columns-1,Utils.sortDescending);
 
-                System.Console.WriteLine($"would have sorted on row {sheet.Dimension.End.Row} & column {sheet.Dimension.End.Column}");
             }
-            // code goes here to recalulate
+
+            // now re-enter all the formulas and recalculate
+            columns = sheet.Dimension.End.Column;
+            for (int i=2;i<=sheet.Dimension.End.Row;i++)
+            {
+                var thisMonthCell = sheet.Cells[i,columns-2].Address;
+                var lastMonthCell = sheet.Cells[i,columns-3].Address;
+                sheet.Cells[i,columns-1].Formula = $"{thisMonthCell}-{lastMonthCell}";
+                sheet.Cells[i,columns-1].Style.Numberformat.Format = "\"$\"#,##0.00";
+            }
+            sheet.Calculate();
+
+            for (int i=2;i<=sheet.Dimension.End.Row;i++)
+            {
+                var changeCell = sheet.Cells[i,columns-1].Address;
+               
+                var lastMonthCell = sheet.Cells[i,columns-3].Address;
+                sheet.Cells[i,columns].Formula = $"{changeCell}/{lastMonthCell}%";
+                sheet.Cells[i,columns].Style.Numberformat.Format = "0.00";
+            }
+            sheet.Calculate();
+
+
 
 
             // delete all rows past row 21
-            /* for (int i=sheet.Dimension.End.Row;i>21;i--)
+             for (int i=sheet.Dimension.End.Row;i>21;i--)
             {
                 sheet.DeleteRow(i);
             }
-            // delete all the columns except for the first one and the last two
+            // delete all the columns except for the first one and the last five (so we see three month spending trend)
             // need to delete this many columns starting at column 2 - we should be able to delete column 2 this many times.
-            int columnsToDelete = sheet.Dimension.End.Column - 3;
+            int columnsToDelete = sheet.Dimension.End.Column - 6;
 
             for (int i=0;i<columnsToDelete;i++)
             {
                 sheet.DeleteColumn(2);
             }
-            */
+            
 
 
         }
